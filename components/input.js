58f46a95e84514c85bbf461863581fa2eb76130d
solk10 <sol.1919@yahoo.com>
2018-08-28 21:38:20 +0300

    clock setting..not finished

--- components/input.js
+++ components/input.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Input = () => (
+export class Input extends React.Component {
+  render(){
+    return(
   <div id="input">
     <form>
       <input type="text" value="username" />
     </form>
   </div>
-);
+)
+}
+}
