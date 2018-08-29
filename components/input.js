f77c06d0f922ece59aa2bc296c09b96fc083209b
solk10 <sol.1919@yahoo.com>
2018-08-29 16:28:02 +0300

    api not completd

--- components/input.js
+++ components/input.js
@@ -1,13 +1,18 @@
 import React from "react";
+import {getUser} from "../utils/getUserData";
 
 export class Input extends React.Component {
   render() {
     return (
       <div id="input">
         <form>
-          <input type="text" value="username" />
+          <input id="username" type="text" value={this.props.username}/>
         </form>
       </div>
     );
   }
 }
+
+
+//ref="" onChange={this.handleChange}//
+//autoFocus autoComplete="false"//
