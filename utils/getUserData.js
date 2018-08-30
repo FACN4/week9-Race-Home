ccc28f624e33fb05b577c482dd61c6d98bd0fc3a
Noor Afifi <noor261998afifi@gmail.com>
2018-08-30 12:04:54 +0300

    20#

--- utils/getUserData.js
+++ utils/getUserData.js
@@ -1,15 +1,13 @@
 //UserName & Avatar_Url//
 import React from "react";
 import ReactDOM from "react-dom";
-import Token from "../token";
-
-const URL = "https://api.github.com" ;
+const URL = "https://api.github.com";
 const getUser = username => {
   const URL = `${BASE_URL}/users/${username}`;
   return fetch(URL) // eslint-disable-line no-undef
     .then(res => {
       if (res.status !== 200) {
-        throw new Error('Non-200 API response');
+        throw new Error("Non-200 API response");
       }
       return res.json();
     });
